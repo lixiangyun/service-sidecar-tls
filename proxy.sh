94bd4d051de02fc72be4521f54527029d193932c
攻城狮 <linimbus@126.com>
2019-12-25 21:13:51 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+export ACT_PORT=$1
 
-sed  's/_PORT/$1/g' config.yaml.temp > config.yaml
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport $1 -j REDIRECT --to-ports 15001
+sed  's/_PORT/${ACT_PORT}/g' config.yaml.temp > config.yaml
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport ${ACT_PORT} -j REDIRECT --to-ports 15001
 ./tcpproxy -config config.yaml
