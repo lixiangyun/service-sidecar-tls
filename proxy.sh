bd19541a725fdb821f1d010661c4152f1c4b5922
攻城狮 <linimbus@126.com>
2019-12-25 21:02:10 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,4 +2,4 @@
 
 sed  's/_PORT/$1/g' config.yaml.temp > config.yaml
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport $1 -j REDIRECT --to-ports 15001
-tcpproxy -config config.yaml
+./tcpproxy -config config.yaml
